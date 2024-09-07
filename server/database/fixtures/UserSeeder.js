2f65c4fafa5673ee6795e0169b05f8681f482949
stevenbachimont <stevenbachimont@gmail.com>
2024-09-07 16:52:31 +0200

    maj bdd

--- server/database/fixtures/UserSeeder.js
+++ server/database/fixtures/UserSeeder.js
@@ -14,14 +14,14 @@ class UserSeeder extends AbstractSeeder {
           "$argon2id$v=19$m=19456,t=2,p=1$rQcSnCLNYupL72R1/tzplQ$NImvLcK++gA/aTygbIzpWcBE6z21+kCccHcNomR6xBg",
         profile_picture: "http://localhost:3310/uploadsAvatars/un.jpg",
         is_admin: 0,
-        is_verify: 0,
+        is_verify: 1,
       },
       {
         alias: "kiki",
         email: "kiki@example.com",
         hashed_password:
           "$argon2id$v=19$m=19456,t=2,p=1$Vcy/8DFEKd2/aiCXB8ERnQ$VXq2S0ln0XJKTsds7XDKHpoT3wIzSNQAQ5QJTXrg22U",
-        profile_picture: "http://localhost:3310/uploadsAvatars/cinq.jpg",
+        profile_picture: "http://localhost:3310/uploadsAvatars/kiki.png",
         is_admin: 1,
         is_verify: 1,
       },
