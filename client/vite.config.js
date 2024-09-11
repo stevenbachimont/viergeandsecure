25e44ea4e7ce7809619ec6b69d416972451e8d7f
stevenbachimont <stevenbachimont@gmail.com>
2024-09-11 20:46:37 +0200

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/test/",
   server: {
     port: 3310,
   },
--- docker-entry.sh
+++ docker-entry.sh
@@ -2,11 +2,6 @@
 
 # Attendre que le service de base de données soit prêt
 
-until nc -z -v -w30 $DB_HOST $DB_PORT
-do
- echo "Waiting for database connection..."
-   sleep 5
-done
 
 
 cd ./server && node ./bin/migrate.js && node ./bin/seed.js && node index.js
